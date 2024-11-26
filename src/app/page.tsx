affe70046bf316df8c2870b4c96d4b9e57dc5a54
Andrey Zhukov <gudvin109@gmail.com>
2024-11-26 23:40:32 +0300

    26112640

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,23 @@
 'use client'
 
 import Container from './components/Container'
-import { useEffect, useState } from 'react'
+
 import Main from './components/Main'
 import { getUserById } from './actions/getUserById'
 import { AppRoot } from '@telegram-apps/telegram-ui'
 import { useInitData } from '@vkruglikov/react-telegram-web-app'
 
-export default function Home() {
-	const [loading, setLoading] = useState(true)
-	const [user, setUser] = useState<any>(null)
+export default async function Home() {
+	//const [user, setUser] = useState<any>(null)
 	const [initDataUnsafe] = useInitData()
 	const telegramId = initDataUnsafe?.user?.id.toString()
-	useEffect(() => {
-		const fetchUser = async () => {
-			if (telegramId) {
-				try {
-					const userData = await getUserById(telegramId) // передаем идентификатор
-					setUser(userData)
-				} catch (error) {
-					console.error('Ошибка при загрузке пользователя:', error)
-				} finally {
-					setLoading(false)
-				}
-			}
-		}
 
-		fetchUser()
-	}, [telegramId])
+	if (!telegramId) {
+		// Возвращаем или показываем ошибку, если telegramId не существует
+		return <div>Ошибка: Не удалось получить Telegram ID</div>
+	}
+
+	const user = await getUserById(telegramId)
 
 	return (
 		<AppRoot>
